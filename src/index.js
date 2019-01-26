ea4b6587b051e613534d97add0e5ccfa59ffdd98
Pablo Lorenzo <lorenz3192@gmail.com>
2019-01-26 05:41:04 -0800

    exported components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import React      from "react";
-import { render } from "react-dom";
-import App        from "./App";
+//import React      from "react";
+//import { render } from "react-dom";
+//import App        from "./App";
+//
+//render(<App/>, window.document.querySelector(".app"));
 
-render(<App/>, window.document.querySelector(".app"));
+export { default as Line } from "./Line/";
+export { default as XAxis } from "./XAxis/";
+export { default as YAxis } from "./YAxis/";
