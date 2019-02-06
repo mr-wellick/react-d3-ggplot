87376d232b7ab4ff4664d8ec44a35931d0fc1e2c
Pablo Lorenzo <lorenz3192@gmail.com>
2019-02-06 07:30:45 -0800

    added code of conduct

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-export { default as Line }   from "./Line/";
-export { default as Points } from "./Points/";
-export { default as Rects }  from "./Rects/";
-export { default as XAxis }  from "./XAxis/";
-export { default as YAxis }  from "./YAxis/";
+export { Line }   from "./Line/";
+export { Points } from "./Points/";
+export { Rects }  from "./Rects/";
+export { XAxis }  from "./XAxis/";
+export { YAxis }  from "./YAxis/";
