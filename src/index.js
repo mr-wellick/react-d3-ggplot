bc917127485d9fceca9c8b00944d820fcfc38a14
Pablo Lorenzo <lorenz3192@gmail.com>
2019-02-05 19:55:05 -0800

    added index.js files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import Line   from "./Line/Line.js";
-import Points from "./Points.js";
-import Rects  from "./Rects/Rects.js";
-import XAxis  from "./XAxis/XAxis.js";
-import YAxis  from "./YAxis/YAxis.js";
-
-export { Line };
-export { Points };
-export { Rects };
-export { XAxis };
-export { YAxis };
+export { default as Line }   from "./Line/";
+export { default as Points } from "./Points/";
+export { default as Rects }  from "./Rects/";
+export { default as XAxis }  from "./XAxis/";
+export { default as YAxis }  from "./YAxis/";
