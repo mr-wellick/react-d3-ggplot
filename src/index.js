b68ff00924fb4e0d78e270019bf08a0fccf048bd
Pablo Lorenzo <lorenz3192@gmail.com>
2019-02-08 16:07:17 -0800

    added Labels to exports

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
-export { Line }   from "./Line/";
-export { Points } from "./Points/";
-export { Rects }  from "./Rects/";
+// base
 export { XAxis }  from "./XAxis/";
 export { YAxis }  from "./YAxis/";
+export { Labels } from "./Labels/";
+
+// shapes
+export { Points } from "./Points/";
+export { Rects }  from "./Rects/";
+export { Line }   from "./Line/";
+
+// Extensions
 export { Color }  from "./Color/";
 export { GGPLOT } from "./GGPLOT/";
