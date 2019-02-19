8a0e316049dfb8237777a7441ecf079cc2545183
Pablo Lorenzo <lorenz3192@gmail.com>
2019-02-19 09:04:25 -0800

    added warning to FACETS

--- src/FACETS/FACETS.js
+++ src/FACETS/FACETS.js
@@ -4,9 +4,24 @@ import { Fragment }  from "react";
 import { GGPLOT }    from "../GGPLOT/";
 
 class FACETS extends Component {
+
+    checkLengths(){
+        const not_enough_data = this.props.data.filter(dataset => dataset.length <= 10);
+
+        return not_enough_data;
+    }
+
     render(){
         // keep datasets with 10 or more entries
         const new_data = this.props.data.filter(dataset => dataset.length >= 10);
+
+        if(this.checkLengths().length > 0)
+        {
+            console.warn(
+                `You have passed in datasets without enough entries.
+                Will only keep datasets with 10 or more entries.`
+            );
+        }
 
         return(
             <Fragment>
@@ -15,8 +30,7 @@ class FACETS extends Component {
                         <GGPLOT
                             { ...this.props }
                             key={ index }
-                            data={ dataset }
-                            className={ this.props.className + "_" + index }
+                            data={ dataset } // override old data with only one dataset
                         >
                             { this.props.children }
                         </GGPLOT>
--- src/__development__/App.js
+++ src/__development__/App.js
@@ -2,25 +2,26 @@ import React         from "react";
 import { Component } from "react";
 import { hot }       from "react-hot-loader";
 import { GGPLOT }    from "../GGPLOT/";
+import { FACETS }    from "../FACETS/";
 import { Points }    from "../Points/";
-//import { nest }      from "d3-collection";
+import { nest }      from "d3-collection";
 import mpg           from "./Data/mpg.json";
 
 class App extends Component {
 
-    //formatData(){
-    //    // this subsets our data by year (there are two years in our dataset: 1999 and 2008)
-    //     const subset = nest().key(d => d["year"]).entries(mpg);
+    formatData(){
+        // this subsets our data by year (there are two years in our dataset: 1999 and 2008)
+         const subset = nest().key(d => d["cyl"]).entries(mpg);
 
-    //     // here, we want only the rawData. This returns an array with two arrays. One array
-    //     // for each category. Since we have two years, 1999 & 2008, we have two arrays.
-    //     const rawData = subset.map(item => item["values"]);
+         // here, we want only the rawData. This returns an array with two arrays. One array
+         // for each category. Since we have two years, 1999 & 2008, we have two arrays.
+         const rawData = subset.map(item => item["values"]);
 
-    //    return rawData;
-    //}
+        return rawData;
+    }
 
     state = {
-        data: mpg,
+        data: this.formatData(),
         aes: ["displ", "hwy"],
         dimensions:
         {
@@ -43,9 +44,9 @@ class App extends Component {
             return <h1>No data to render.</h1>;
 
         return(
-            <GGPLOT { ...this.state }>
-                <Points var_name="class"/>
-            </GGPLOT>
+            <FACETS { ...this.state }>
+                <Points/>
+            </FACETS>
         );
     }
 
