a2925fd71c7cf057d50b655636b589e8da71d829
Pablo Lorenzo <lorenz3192@gmail.com>
2019-03-02 07:41:09 -0800

    Added GEOMS, which allows you to create custom graphs

--- src/__development__/App.js
+++ src/__development__/App.js
@@ -1,32 +1,38 @@
 import React         from "react";
 import { Component } from "react";
 import { hot }       from "react-hot-loader";
-import { GGPLOT }    from "../GGPLOT/";
-import { FACETS }    from "../FACETS/";
+//import { GGPLOT }    from "../GGPLOT/";
+//import { FACETS }    from "../FACETS/";
+//import { nest }      from "d3-collection";
+import { Background } from "../Background/";
+import { XGrid }     from "../XGrid/";
+import { YGrid }     from "../YGrid/";
+import { GEOMS }     from "../GEOMS/";
+import { XAxis }     from "../XAxis/";
+import { YAxis }     from "../YAxis/";
 import { Points }    from "../Points/";
-import { nest }      from "d3-collection";
 import mpg           from "./Data/mpg.json";
 
 class App extends Component {
 
-    formatData(){
-        // this subsets our data by year (there are two years in our dataset: 1999 and 2008)
-         const subset = nest().key(d => d["cyl"]).entries(mpg);
+    //formatData(){
+    //    // this subsets our data by year (there are two years in our dataset: 1999 and 2008)
+    //     const subset = nest().key(d => d["cyl"]).entries(mpg);
 
-         // here, we want only the rawData. This returns an array with two arrays. One array
-         // for each category. Since we have two years, 1999 & 2008, we have two arrays.
-         const rawData = subset.map(item => item["values"]);
+    //     // here, we want only the rawData. This returns an array with two arrays. One array
+    //     // for each category. Since we have two years, 1999 & 2008, we have two arrays.
+    //     const rawData = subset.map(item => item["values"]);
 
-        return rawData;
-    }
+    //    return rawData;
+    //}
 
     state = {
-        data: this.formatData(),
+        data: mpg,
         aes: ["displ", "hwy"],
         dimensions:
         {
             width: window.innerWidth*0.8,
-            height: window.innerHeight*0.6,
+            height: window.innerHeight*0.9,
             padding: 50
         }
     }
@@ -35,7 +41,7 @@ class App extends Component {
         dimensions: {
             ...this.state.dimensions,
             width: window.innerWidth*0.8,
-            height: window.innerHeight*0.6
+            height: window.innerHeight*0.9
         }
     });
 
@@ -44,9 +50,14 @@ class App extends Component {
             return <h1>No data to render.</h1>;
 
         return(
-            <FACETS { ...this.state }>
+            <GEOMS { ...this.state }>
+                <Background/>
+                <XGrid/>
+                <YGrid/>
+                <XAxis/>
+                <YAxis/>
                 <Points/>
-            </FACETS>
+            </GEOMS>
         );
     }
 
