d054c013b7e4d83e97fb76e4c2a5b2e7933c0f12
Pablo Lorenzo <lorenz3192@gmail.com>
2019-04-01 13:58:14 -0700

    refactored to use react-hooks

--- src/Line/Line.js
+++ src/Line/Line.js
@@ -1,71 +1,39 @@
 import React               from "react";
-import { Component }       from "react";
-import PropTypes           from "prop-types";
+import { useRef }          from "react";
+import { useEffect }       from "react";
+import { useContext }      from "react";
+import { ChartContext }    from "../_context/";
+import { useScale }        from "../_hooks/";
+import { select }          from "d3-selection";
 import { line }            from "d3-shape";
 import { curveCatmullRom } from "d3-shape";
-import { select }          from "d3-selection";
-import { ScalesConsumer }  from "../Context/";
 
-class Line extends Component {
-    static contextType = ScalesConsumer;
+function Line(props) {
+    const node    = useRef(null);
+    const context = useContext(ChartContext);
+    const xScale  = useScale(context, "XAxis");
+    const yScale  = useScale(context, "YAxis");
 
-    static defaultProps = {
-        color: "black",
-        opacity: 1,
-        lineWidth: 1
-    }
-
-    static propTypes = {
-        color: PropTypes.string,
-        lineWidth: PropTypes.number,
-        opacity: PropTypes.any,
-        createScaleType: PropTypes.func
-    }
-
-    createLine() {
-        const { aes } = this.context;
-        const xScale  = this.props.createScaleType(aes[0], "XAxis"); // We need to pass in XAxis & YAxis
-        const yScale  = this.props.createScaleType(aes[1], "YAxis"); // to properly display the line visually.
-
-        // create line for chart
-        const chartLine = line()
-            .x(d => xScale(d[aes[0]]))
-            .y(d => yScale(d[aes[1]]))
+    useEffect(() => {
+        const lineToAppend = line()
+            .x(d => xScale(d[context.aes[0]]))
+            .y(d => yScale(d[context.aes[1]]))
             .curve(curveCatmullRom);
 
-        return chartLine;
-    }
-
-    appendLine() {
-        const { color, lineWidth, opacity } = this.props;
-        const { data }                      = this.context;
-        const lineToAppend                  = this.createLine();
-
         // append line to plot
-        select(this.node)
-            .datum(data)
+        select(node.current)
+            .datum(context.data)
             .attr("fill", "none")
-            .attr("stroke", color)
-            .attr("stroke-width", lineWidth)
-            .attr("d", lineToAppend)
-            .attr("opacity", opacity);
-    }
+            .attr("stroke", "black")
+            .attr("stroke-width", "1")
+            .attr("d", lineToAppend);
+    });
 
-    render() {
-        return(
-            <g> 
-                <path ref={ node => this.node = node }/>
-            </g>
-        );
-    }
-
-    componentDidMount() {
-        this.appendLine();
-    }
-
-    componentDidUpdate() {
-        this.appendLine();
-    }
+    return(
+        <g>
+            <path ref={ node }></path>
+        </g>
+    );
 }
 
 export default Line;
--- src/__development__/App.js
+++ src/__development__/App.js
@@ -1,13 +1,15 @@
-import React        from "react";
-import { useState } from "react";
-import { GGPLOT }   from "../GGPLOT/";
-import { Points }   from "../Points/";
-import mpg          from "./Data/mpg.json";
+import React         from "react";
+import { useState }  from "react";
+import { useEffect } from "react";
+import { GGPLOT }    from "../GGPLOT/";
+//import { Points }    from "../Points/";
+import { Line }      from "../Line/";
+//import mpg           from "./Data/mpg.json";
 
 function App() {
-    const [state] = useState({
-        data: mpg,
-        aes: ["displ", "hwy"],
+    const [state, setState] = useState({
+        data: [],
+        aes: ["date", "close"],
         dimensions:
         {
             width: window.innerWidth*0.8,
@@ -16,9 +18,31 @@ function App() {
         }
     });
 
+    useEffect(() => {
+        fetch("https://api.iextrading.com/1.0/stock/aapl/chart/5y")
+            .then(res => res.json())
+            .then(data => {
+                // format date to JS date objects
+                const formattedData = data.map(item => ({
+                    ...item,
+                    date: new Date(item.date)
+                }));
+                // update state with correctly formatted data
+                setState({
+                    ...state,
+                    data: formattedData
+                });
+            });
+    }, []);
+
+    console.log(state);
+
+    if(state.data.length === 0)
+        return <h1>Loading...</h1>;
+
     return(
         <GGPLOT { ...state }>
-            <Points/>
+            <Line/>
         </GGPLOT>
     );
 }
