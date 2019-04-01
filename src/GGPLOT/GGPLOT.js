8bca4311cd6de8372cab078efb327f68d20612cc
Pablo Lorenzo <lorenz3192@gmail.com>
2019-04-01 09:15:47 -0700

    coverted YAxis to use react-hooks

--- src/GGPLOT/GGPLOT.js
+++ src/GGPLOT/GGPLOT.js
@@ -2,12 +2,14 @@ import React            from "react";
 import PropTypes        from "prop-types";
 import { ChartContext } from "../_context/";
 import { XAxis }        from "../XAxis/";
+import { YAxis }        from "../YAxis/";
 
 function GGPLOT(props) {
         return(
             <ChartContext.Provider value={ props }>
                 <svg width={ props.dimensions.width } height={ props.dimensions.height }>
                     <XAxis/>
+                    <YAxis/>
                 </svg>
             </ChartContext.Provider>
         );
