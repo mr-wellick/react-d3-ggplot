1e16a010f4085b70602b8c4412e50fdd45d48786
Pablo Lorenzo <lorenz3192@gmail.com>
2019-04-21 18:27:34 -0700

    code formatting

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,28 @@
 // Base webpack config
-let path     = require("path");
-let webpack  = require("webpack");
-let merge    = require("webpack-merge");
-let template = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require("webpack");
+const merge = require("webpack-merge");
+const template = require("html-webpack-plugin");
 
 // Development, production, and presets
-let configType = (env) => require(`./build-utils/webpack.${env}`)(env);
+const configType = env =>
+  require(`./build-utils/webpack.${env}`)(env);
 
 // Final webpack configuration
-module.exports = ( { mode } = { mode: "production" } ) => {
-    return merge(
-        {
-            mode,
-            entry: path.join(__dirname, "./src/__development__/index.js"),
-            output:
-            {
-                path: path.join(__dirname, "build"),
-                filename: "index.js"
-            },
-            plugins:
-            [
-                new template({ template: "src/__development__/index.html" }),
-                new webpack.ProgressPlugin()
-            ]
-        },
-        configType(mode)
-    );
+module.exports = ({ mode } = { mode: "production" }) => {
+  return merge(
+    {
+      mode,
+      entry: path.join(__dirname, "./src/__development__/index.js"),
+      output: {
+        path: path.join(__dirname, "build"),
+        filename: "index.js"
+      },
+      plugins: [
+        new template({ template: "src/__development__/index.html" }),
+        new webpack.ProgressPlugin()
+      ]
+    },
+    configType(mode)
+  );
 };
