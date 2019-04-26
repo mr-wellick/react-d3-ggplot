91176934241eda87f45a166b69b0e2cf3b07dbd0
Pablo Lorenzo <lorenz3192@gmail.com>
2019-04-26 11:35:47 -0700

    added error handling for incorrect component name passed in

--- src/_hooks/useScale.ts
+++ src/_hooks/useScale.ts
@@ -1,15 +1,24 @@
 import { ScaleFinder } from "../utilities/";
+import { IAppContext } from "../_context/ChartContext";
 
-function findScale(context, componentName: string) {
-  const keyToUse =
-    componentName === "XAxis" || componentName === "XGrid" ? context.aes[0] : context.aes[1];
+function findScale(context: IAppContext, componentName: string) {
+  let keyToUse: string;
+
+  if (componentName === "XAxis" || componentName === "XGrid") {
+    keyToUse = context.aes[0];
+  } else if (componentName === "YAxis" || componentName === "YGrid") {
+    keyToUse = context.aes[1];
+  } else {
+    throw new Error("An invalid component name, used to calculate the scale, was used.");
+  }
+
   const values = context.data.map(item => item[keyToUse]);
   const scale = new ScaleFinder(values);
 
   return scale;
 }
 
-function useScale(context, componentName: string) {
+function useScale(context: IAppContext, componentName: string) {
   const scale = findScale(context, componentName);
 
   // find appropiate scale type
