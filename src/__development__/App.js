4edc1863b65707e757758a416cd2b483b4c64f19
Pablo Lorenzo <lorenz3192@gmail.com>
2019-04-26 09:52:06 -0700

    restructured App.js

--- src/__development__/App.js
+++ src/__development__/App.js
@@ -1,32 +1,26 @@
 import React from "react";
 import { useState } from "react";
-//import { useEffect }  from "react";
-//import { GGPLOT }     from "../GGPLOT/";
-//import { GEOMS }      from "../GEOMS/";
-import { FACETS } from "../FACETS/";
-//import { Background } from "../Background/";
-//import { XGrid }      from "../XGrid/";
-//import { YGrid }      from "../YGrid/";
-//import { XAxis }      from "../XAxis/";
-//import { YAxis }      from "../YAxis/";
-//import { Line }       from "../Line/";
+//import { useEffect } from "react";
+import { Background } from "../Background/";
+import { GGPLOT } from "../GGPLOT/";
+//import { Line } from "../Line/";
 import { Points } from "../Points/";
-//import { Rects }      from "../Rects/";
+//import { Rects } from "../Rects/";
 import mpg from "./Data/mpg.json";
-import { nest } from "d3-collection";
+//import { nest } from "d3-collection";
 
-function formatData() {
-  const subset = nest()
-    .key(d => d["year"])
-    .entries(mpg);
-  const rawData = subset.map(item => item["values"]);
-
-  return rawData;
-}
+//function formatData() {
+//  const subset = nest()
+//    .key(d => d["year"])
+//    .entries(mpg);
+//  const rawData = subset.map(item => item["values"]);
+//
+//  return rawData;
+//}
 
 function App() {
   const [state] = useState({
-    data: formatData(),
+    data: mpg,
     aes: ["displ", "hwy"],
     dimensions: {
       width: window.innerWidth * 0.8,
@@ -36,11 +30,9 @@ function App() {
   });
 
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
-      <FACETS {...state}>
-        <Points />
-      </FACETS>
-    </div>
+    <GGPLOT {...state}>
+      <Points />
+    </GGPLOT>
   );
 }
 
