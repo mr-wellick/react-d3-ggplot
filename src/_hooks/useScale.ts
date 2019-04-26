0d005215d1aa147811bd2f45ee4798da1e38cc33
Pablo Lorenzo <lorenz3192@gmail.com>
2019-04-26 11:18:34 -0700

    added findScale function

---
+++ src/_hooks/useScale.ts
@@ -0,0 +1,42 @@
+import { ScaleFinder } from "../utilities/";
+
+function findScale(context, componentName: string) {
+  const keyToUse =
+    componentName === "XAxis" || componentName === "XGrid" ? context.aes[0] : context.aes[1];
+  const values = context.data.map(item => item[keyToUse]);
+  const scale = new ScaleFinder(values);
+
+  return scale;
+}
+
+function useScale(context, componentName: string) {
+  const scale = findScale(context, componentName);
+
+  // find appropiate scale type
+  let scaleType;
+
+  if (typeof scale.data[0] === "number") {
+    scaleType = scale.getLinearScale().nice();
+  }
+
+  if (typeof scale.data[0] === "object") {
+    scaleType = scale.getTimeScale().nice();
+  }
+
+  if (typeof scale.data[0] === "string") {
+    scaleType = scale.getOrdinalScale(0.5);
+  }
+
+  // we need to visually spread our points
+  const { width, height, padding } = context.dimensions;
+
+  if (componentName === "XAxis" || componentName === "XGrid") {
+    scaleType.range([padding, width - padding]);
+  } else if (componentName === "YAxis" || componentName === "YGrid") {
+    scaleType.range([height - padding, padding]);
+  }
+
+  return scaleType;
+}
+
+export default useScale;
