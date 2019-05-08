724852f735455c4f64096487ee48a2029f165cd9
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-08 06:02:13 -0700

    Refactored to allow use of references in selections

--- src/XAxis/XAxis.tsx
+++ src/XAxis/XAxis.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { useContext } from "react";
 import { useEffect } from "react";
+import { useRef } from "react";
 import { ChartContext } from "../_context/";
-import { IAppContext } from "../_context/";
+import { IContext } from "../_context/";
 import { useScale } from "../_hooks/";
 import { select } from "d3-selection";
 import { axisBottom } from "d3-axis";
@@ -12,18 +13,27 @@ interface IProps {
   label: string;
 }
 
+// componet using application context
 function XAxis(props: IProps) {
-  const context = useContext<IAppContext>(ChartContext);
-  const scale: any = useScale(context, "XAxis");
+  // here we use any so we can it in when select(). if we don't, we can only use a STRING to select node.
+  // And we don't want that since we'll be accidentally toucing other <g> elements
+  const ref: any = useRef(null);
+  const context = useContext<IContext>(ChartContext);
+  const scale = useScale(context, "XAxis");
 
   useEffect(() => {
     const { dimensions, aes, data } = context;
     const axisLocation = `translate(0, ${dimensions.height - dimensions.padding})`;
 
     // select node returned by component and appends x-axis
-    const node = select<SVGGElement, string | number | object>("g")
-      .attr("transform", axisLocation)
-      .call(axisBottom(scale));
+    const node = select<SVGGElement, number | Date | string>(ref.current).attr(
+      "transform",
+      axisLocation
+    );
+
+    if (scale !== undefined) {
+      node.call(axisBottom(scale));
+    }
 
     // format x-labels
     if (typeof data[0][aes[0]] === "number" && props.label !== undefined) {
@@ -31,7 +41,7 @@ function XAxis(props: IProps) {
     }
   });
 
-  return <g />;
+  return <g ref={ref} />;
 }
 
 export default XAxis;
