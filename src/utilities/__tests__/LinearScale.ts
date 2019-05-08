6953aa754ea32759026af2d48309ee914307870d
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-08 07:06:43 -0700

    successfully added testing for TypeScript

--- src/utilities/__tests__/LinearScale.ts
+++ src/utilities/__tests__/LinearScale.ts
@@ -1,5 +1,9 @@
 import LinearScale from "../LinearScale";
+import { Numeric } from "d3-array";
 
-test("test", () => {
-  console.log("hello");
+// testing data for linear scale
+const data: Numeric[] = [1.8, 1.8, 2, 2, 5.7, 6, 5.7, 5.3, 5.3, 5.7, 6.5, 4.6, 4.6];
+
+test("initialization of LinearScale", () => {
+  console.log("I am a test component");
 });
