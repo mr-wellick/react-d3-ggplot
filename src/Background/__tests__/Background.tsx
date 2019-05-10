94c9ca76182477b68c4e1d98662bc2b073827d28
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-10 15:52:17 -0700

    added unit testing for Background

---
+++ src/Background/__tests__/Background.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Background from "../Background";
+import { GEOMS } from "../../GEOMS";
+import { render } from "react-testing-library";
+import { cleanup } from "react-testing-library";
+
+afterEach(cleanup);
+
+// here we only care about the dimensions property.
+// <Background /> only needs dimensions object
+const validState = {
+  data: [],
+  aes: [],
+  dimensions: { width: 500, height: 300, padding: 50 }
+};
+
+test("<Background/> renders rect element", () => {
+  // <Background/> is not intended to be used as a stand-alone componet, yet.
+  // When using background, users must use it within <GEOMS/>
+  const { container } = render(
+    <GEOMS {...validState}>
+      <Background />
+    </GEOMS>
+  );
+});
