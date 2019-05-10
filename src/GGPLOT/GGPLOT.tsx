056d37f00c8bb2272f4eaddea0bd736b2301d8bc
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-10 13:52:13 -0700

    Converted GGPLOT to TypeScript
    Commented out XGrid and YGrid both of which are being rebuilt

--- src/GGPLOT/GGPLOT.tsx
+++ src/GGPLOT/GGPLOT.tsx
@@ -1,21 +1,28 @@
-import React from "react";
+import React, { ReactChild } from "react";
 import PropTypes from "prop-types";
 import { ChartContext } from "../_context/";
+import { IContext } from "../_context";
 import { Background } from "../Background/";
 import { XAxis } from "../XAxis/";
 import { YAxis } from "../YAxis/";
-import { XGrid } from "../XGrid/";
-import { YGrid } from "../YGrid/";
+//import { XGrid } from "../XGrid/";
+//import { YGrid } from "../YGrid/";
 
-function GGPLOT(props) {
+interface IProps extends IContext {
+  children: ReactChild;
+}
+
+function GGPLOT(props: IProps) {
   return (
     <ChartContext.Provider value={props}>
       <svg width={props.dimensions.width} height={props.dimensions.height}>
         <Background />
         <XAxis />
         <YAxis />
+        {/**
         <XGrid />
         <YGrid />
+         */}
         {props.children}
       </svg>
     </ChartContext.Provider>
