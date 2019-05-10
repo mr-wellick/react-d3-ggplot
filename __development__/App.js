5eaabb3cf4b3a17b955810866ca2d9251fe3f473
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-10 09:48:06 -0700

    changes in here are not important

--- __development__/App.js
+++ __development__/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import { useState } from "react";
+import { useEffect } from "react";
 import { GEOMS } from "../src/GEOMS";
 import { Background } from "../src/Background";
 import { XAxis } from "../src/XAxis";
 import { YAxis } from "../src/YAxis";
 import { Points } from "../src/Points";
+import { Rects } from "../src/Rects";
+import { Line } from "../src/Line";
 import mpg from "./Data/mpg.json";
 //import { nest } from "d3-collection";
 
@@ -18,15 +21,33 @@ import mpg from "./Data/mpg.json";
 //}
 
 function App() {
-  const [state] = useState({
-    data: mpg,
-    aes: ["displ", "hwy"],
+  const [state, setState] = useState({
+    data: [],
+    aes: ["date", "close"],
     dimensions: {
       width: window.innerWidth * 0.8,
       height: window.innerHeight * 0.9,
       padding: 50
     }
   });
+
+  useEffect(() => {
+    fetch("https://api.iextrading.com/1.0/stock/aapl/chart/5y")
+      .then(res => res.json())
+      .then(data => {
+        // format date to JS date objects
+        const formattedData = data.map(item => ({
+          ...item,
+          date: new Date(item.date)
+        }));
+        // update state with correctly formatted data
+        setState({ ...state, data: formattedData });
+      });
+  }, []);
+
+  if (state.data.length === 0) {
+    return <h1>Loading...</h1>;
+  }
 
   return (
     <>
@@ -36,7 +57,7 @@ function App() {
         <Background />
         <YAxis />
         <XAxis />
-        <Points />
+        <Line />
       </GEOMS>
     </>
   );
