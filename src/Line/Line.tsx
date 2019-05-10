9d7af0ea56718c45de179a897a20f492f4c9384e
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-10 09:43:19 -0700

    converted to typescript

---
+++ src/Line/Line.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useContext } from "react";
+import { ChartContext } from "../_context/";
+import { useScale } from "../_hooks/";
+import { line } from "d3-shape";
+import { curveCatmullRom } from "d3-shape";
+
+interface IProps {
+  fill?: string;
+  width?: string;
+  color?: string;
+}
+
+function Line(props: IProps) {
+  const context = useContext(ChartContext);
+  const xScale: any = useScale(context, "XAxis");
+  const yScale: any = useScale(context, "YAxis");
+
+  const lineToAppend = line()
+    .x((d: any) => xScale(d[context.aes[0]]))
+    .y((d: any) => yScale(d[context.aes[1]]))
+    .curve(curveCatmullRom)(context.data);
+
+  return (
+    <g>
+      <path
+        fill="none"
+        stroke={!props.color ? "crimson" : props.color}
+        strokeWidth={!props.width ? "1" : props.width}
+        d={!lineToAppend ? undefined : lineToAppend}
+      />
+    </g>
+  );
+}
+
+export default Line;
