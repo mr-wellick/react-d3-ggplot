32fa52da64d8757bd7f832d12c1c62f876b65ebb
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-10 09:46:56 -0700

    Added explicit typings
    Altough our typings are correct, D3 want an exact match. We can't have
    CorrectType1 | CorrectType2

--- src/XAxis/XAxis.tsx
+++ src/XAxis/XAxis.tsx
@@ -18,7 +18,7 @@ function XAxis(props: IProps) {
   // And we don't want that since we'll be accidentally touching other <g> elements
   const ref: any = useRef(null);
   const context = useContext(ChartContext);
-  const scale = useScale(context, "XAxis");
+  const scale: any = useScale(context, "XAxis");
 
   useEffect(() => {
     const { dimensions, aes, data } = context;
