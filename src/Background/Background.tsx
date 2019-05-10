d8bdb7c477cd72b5630c03b2d2d06dc97453e991
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-10 07:50:03 -0700

    converted to typescript

---
+++ src/Background/Background.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useContext } from "react";
+import { ChartContext } from "../_context/";
+import { IContext } from "../_context/";
+
+function Background(props: IContext) {
+  const { dimensions } = useContext(ChartContext);
+
+  return (
+    <rect
+      width={dimensions.width - dimensions.padding * 2}
+      height={dimensions.height - dimensions.padding * 2}
+      fill="#f1f1f1"
+      transform={`translate(${dimensions.padding}, ${dimensions.padding})`}
+    />
+  );
+}
+
+export default Background;
