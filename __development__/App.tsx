3c0aac76d34912bcf89da3e961ee26ee67322121
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-24 01:10:15 -0700

    changes in here are not important

--- __development__/App.tsx
+++ __development__/App.tsx
@@ -1,30 +1,22 @@
-import React from "react";
+import * as React from "react";
 import { useState } from "react";
-import { useEffect } from "react";
-import { GEOMS } from "../src/GEOMS";
+// import { GEOMS } from "../src/GEOMS";
+// import { XGrid } from "../src/XGrid/";
+// import { YGrid } from "../src/YGrid/";
+// import { Background } from "../src/Background";
+// import { XAxis } from "../src/XAxis";
+// import { YAxis } from "../src/YAxis";
 import { GGPLOT } from "../src/GGPLOT";
-import { Background } from "../src/Background";
-import { XAxis } from "../src/XAxis";
-import { YAxis } from "../src/YAxis";
 import { Points } from "../src/Points";
-import { Rects } from "../src/Rects";
-import { Line } from "../src/Line";
-// import mpg from "./Data/mpg.json";
+// import { Rects } from "../src/Rects";
+// import { Line } from "../src/Line";
 // import { nest } from "d3-collection";
-
-// function formatData() {
-//   const subset = nest()
-//     .key(d => d["year"])
-//     .entries(mpg);
-//   const rawData = subset.map(item => item["values"]);
-//
-//   return rawData;
-// }
+import { mpg } from "./Data/";
 
 function App() {
   const [state, setState] = useState({
-    data: [],
-    aes: ["date", "close"],
+    data: mpg,
+    aes: ["displ", "hwy"],
     dimensions: {
       width: window.innerWidth * 0.8,
       height: window.innerHeight * 0.9,
@@ -32,28 +24,10 @@ function App() {
     }
   });
 
-  useEffect(() => {
-    fetch("https://api.iextrading.com/1.0/stock/aapl/chart/5y")
-      .then(res => res.json())
-      .then(data => {
-        // format date to JS date objects
-        const formattedData = data.map((item: { [key: string]: any }) => ({
-          ...item,
-          date: new Date(item.date)
-        }));
-        // update state with correctly formatted data
-        setState({ ...state, data: formattedData });
-      });
-  }, []);
-
-  if (state.data.length === 0) {
-    return <h1>Loading...</h1>;
-  }
-
   return (
     <>
       <GGPLOT {...state}>
-        <Line />
+        <Points />
       </GGPLOT>
     </>
   );
