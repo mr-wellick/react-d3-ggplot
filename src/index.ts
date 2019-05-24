7db8560280af38d0ec3af1fce3ab31c0853fbf68
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-24 01:24:20 -0700

    exported more components

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
-// base plot
+// base plots
 export { GGPLOT } from "./GGPLOT/";
+export { GEOMS } from "./GEOMS/";
 
 // geoms
 export { Points } from "./Points/";
 export { Rects } from "./Rects/";
 export { Line } from "./Line/";
+
+// graph pieces
+export { XAxis } from "./XAxis/";
+export { XGrid } from "./XGrid/";
+export { YAxis } from "./YAxis/";
+export { YGrid } from "./YGrid/";
+export { Background } from "./Background/";
