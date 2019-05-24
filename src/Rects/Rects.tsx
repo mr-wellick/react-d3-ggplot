8a6585a287de591defea729601522388edb9621f
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-23 18:47:48 -0700

    Refactored and added test
    Refactored Rects component to handle the following type of x-values
    
      - strings
      - JS date objects
      - numbers
    
    Resolves issue #5

--- src/Rects/Rects.tsx
+++ src/Rects/Rects.tsx
@@ -11,24 +11,37 @@ interface IProps {
 Rects.displayName = "Rects";
 
 function Rects(props: IProps) {
-  const context = useContext(ChartContext);
+  const { data, aes, dimensions } = useContext(ChartContext);
   const xScale: any = useXScale();
   const yScale: any = useYScale();
+  let width: any;
 
-  // will change once we start testing
-  if (!xScale || !yScale) {
-    return <h1>whoops</h1>;
+  if (typeof data[0][aes[0]] === "string") {
+    width = xScale.bandwidth();
+  }
+  // this two cases shouldn't be used by the user. these lead to unreadable charts.
+  // however, this code is here so we don't crash
+  else if (typeof data[0][aes[0]] === "number") {
+    console.warn(
+      "If the domain of your x-values is large, this will result in unreadable charts. Consider a different graph."
+    );
+    width = dimensions.width / xScale.ticks().length - dimensions.padding;
+  } else if (typeof data[0][aes[0]] === "object") {
+    console.warn(
+      "If the domain of your x-values is large, this will result in unreadable charts. Consider a different graph."
+    );
+    width = dimensions.width / xScale.ticks().length - dimensions.padding;
   }
 
   return (
     <g>
-      {context.data.map((datum, index) => (
+      {data.map((datum, index) => (
         <rect
           key={index}
-          width={xScale.bandwidth()} // this works for ordinal scales but we need to account for time and number scales as well
-          height={yScale.range()[0] - yScale(datum[context.aes[1]])}
-          x={xScale(datum[context.aes[0]])}
-          y={yScale(datum[context.aes[1]])}
+          width={width}
+          height={yScale.range()[0] - yScale(datum[aes[1]])}
+          x={xScale(datum[aes[0]])}
+          y={yScale(datum[aes[1]])}
           fill={!props.fill ? "cyan" : props.fill}
         />
       ))}
--- src/Rects/__tests__/Rects.tsx
+++ src/Rects/__tests__/Rects.tsx
@@ -6,22 +6,52 @@ import { render, cleanup } from "react-testing-library";
 
 afterEach(cleanup);
 
-// this data is only for an ordinal scale.
-// Iternally, we are using xScale.bandwidth() to calculate the width of each <rect>
-// But that method only exists on ordinal scales. Hence, the x-values being strings for now
-// until we fix this issue
-const validState = {
+const validStateForCategories = {
   data: [{ x: "developer", y: 40 }, { x: "ceo", y: 45 }],
   aes: ["x", "y"],
   dimensions: { width: 500, height: 300, padding: 50 }
 };
 
-test("Rects renders a <g> element wrapping N <rect> elements", () => {
+test("Rects works best if your x-values are categories", () => {
   const { container } = render(
-    <GEOMS {...validState}>
+    <GEOMS {...validStateForCategories}>
       <Rects />
     </GEOMS>
   );
 
   expect(container.firstChild).toMatchSnapshot();
+});
+
+const validStateForNumbers = {
+  data: [{ x: 5, y: 40 }, { x: 1, y: 45 }],
+  aes: ["x", "y"],
+  dimensions: { width: 500, height: 300, padding: 50 }
+};
+
+test("Rects will accept x-values that are numbers", () => {
+  // this will work but users should consider other plots!
+  // the plots that may be rendered can possibly be illegible!
+  // will consider throwing an error instead down the line
+  const { container } = render(
+    <GEOMS {...validStateForNumbers}>
+      <Rects />
+    </GEOMS>
+  );
+});
+
+const validStateForDates = {
+  data: [{ x: new Date(2010), y: 40 }, { x: new Date(2020), y: 45 }],
+  aes: ["x", "y"],
+  dimensions: { width: 500, height: 300, padding: 50 }
+};
+
+test("Rects will accept x-values that are JS date objects", () => {
+  // this will work but users should consider other plots!
+  // the plots that may be rendered can possibly be illegible!
+  // will consider throwing an error instead down the line
+  const { container } = render(
+    <GEOMS {...validStateForDates}>
+      <Rects />
+    </GEOMS>
+  );
 });
