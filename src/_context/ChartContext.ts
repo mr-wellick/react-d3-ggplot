6248cb9065a35aed2191ec9b362e494c62660e61
Pablo Lorenzo <lorenz3192@gmail.com>
2019-05-27 19:52:21 -0700

    Added more props to ChartContext
      - x_format?: allows us to format the x-values
      - y_format?: allows us to format the y-values
      - dimensions?: will now handle dimensions options internally if user
      doesn't specify this option for convenience

--- src/_context/ChartContext.ts
+++ src/_context/ChartContext.ts
@@ -3,13 +3,17 @@ import { createContext } from "react";
 export interface IContext {
   data: any[];
   aes: string[];
-  dimensions: { width: number; height: number; padding: number };
+  dimensions?: { width: number; height: number; padding: number };
+  x_format?: string;
+  y_format?: string;
 }
 
 const ChartContext = createContext<IContext>({
   data: [],
-  aes: ["", ""],
-  dimensions: { width: window.innerWidth, height: window.innerHeight, padding: 50 }
+  aes: [],
+  dimensions: { width: window.innerWidth, height: window.innerHeight, padding: 50 },
+  x_format: "",
+  y_format: ""
 });
 
 export default ChartContext;
