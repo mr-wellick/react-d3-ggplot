04d3045a00cf77acd89e3dc7c115fbb97077c8ef
Pablo Lorenzo <lorenz3192@gmail.com>
2019-06-04 04:56:41 -0700

    testing components

--- development/App.tsx
+++ development/App.tsx
@@ -7,17 +7,26 @@ import { useState } from "react";
 // import { XAxis } from "../src/XAxis";
 // import { YAxis } from "../src/YAxis";
 // import { FACETS } from "../src/FACETS";
-import { GGPLOT } from "../src/GGPLOT";
-import { Points } from "../src/Points";
-// import { Rects } from "../src/Rects";
+// import { Points } from "../src/Points";
 // import { Line } from "../src/Line";
 // import { nest } from "d3-collection";
-import { mpg } from "./Data/";
+// import { mpg } from "./Data/";
+import { GGPLOT } from "../src/GGPLOT";
+import { Rects } from "../src/Rects";
+
+const data = [
+  { x: "a", y: 7 },
+  { x: "b", y: 2 },
+  { x: "c", y: -5 },
+  { x: "d", y: 10 },
+  { x: "e", y: 3 },
+  { x: "f", y: -10 }
+];
 
 function App() {
   const [state, setState] = useState({
-    data: mpg,
-    aes: ["displ", "hwy"],
+    data,
+    aes: ["x", "y"],
     dimensions: {
       width: window.innerWidth * 0.8,
       height: window.innerHeight * 0.9,
@@ -28,7 +37,7 @@ function App() {
   return (
     <>
       <GGPLOT {...state}>
-        <Points />
+        <Rects />
       </GGPLOT>
     </>
   );
