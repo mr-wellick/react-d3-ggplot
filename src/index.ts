0334a5fc5eaf6968955555c6f3da3facc55518ee
Pablo Lorenzo <lorenz3192@gmail.com>
2019-09-02 13:05:02 -0700

    deleted files

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,2 @@
 // base plots
 export { GEOMS } from "./geoms/";
-// export { GGPLOT } from "./GGPLOT/";
-// export { FACETS } from "./FACETS";
-
-// geoms
-// export { Points } from "./Points/";
-// export { Rects } from "./Rects/";
-// export { Line } from "./Line/";
-
-// graph pieces
-// export { XAxis } from "./XAxis/";
-// export { XGrid } from "./XGrid/";
-// export { YAxis } from "./YAxis/";
-// export { YGrid } from "./YGrid/";
-// export { Background } from "./Background/";
