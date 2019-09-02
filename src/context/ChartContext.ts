6c5aa064bd62445ef3aece084d55b8aa316d8ce6
Pablo Lorenzo <lorenz3192@gmail.com>
2019-09-02 13:25:40 -0700

    removed optional

--- src/context/ChartContext.ts
+++ src/context/ChartContext.ts
@@ -3,13 +3,13 @@ import { createContext } from "react";
 export interface IContext {
   data: any[];
   aes: string[];
-  dimensions?: { width?: number; height?: number; padding?: number };
+  dimensions: { width: number; height: number; padding: number };
 }
 
 const ChartContext = createContext<IContext>({
   data: [],
   aes: [],
-  dimensions: {}
+  dimensions: { width: 500, height: 300, padding: 50 }
 });
 
 export default ChartContext;
