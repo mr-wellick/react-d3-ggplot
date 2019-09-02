b4673bc8b829a76d4c32ac64ef6df652c60ec574
Pablo Lorenzo <lorenz3192@gmail.com>
2019-09-02 12:51:00 -0700

    commented out exports

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 // base plots
-export { GGPLOT } from "./GGPLOT/";
 export { GEOMS } from "./GEOMS/";
-export { FACETS } from "./FACETS";
+// export { GGPLOT } from "./GGPLOT/";
+// export { FACETS } from "./FACETS";
 
 // geoms
-export { Points } from "./Points/";
-export { Rects } from "./Rects/";
-export { Line } from "./Line/";
+// export { Points } from "./Points/";
+// export { Rects } from "./Rects/";
+// export { Line } from "./Line/";
 
 // graph pieces
-export { XAxis } from "./XAxis/";
-export { XGrid } from "./XGrid/";
-export { YAxis } from "./YAxis/";
-export { YGrid } from "./YGrid/";
-export { Background } from "./Background/";
+// export { XAxis } from "./XAxis/";
+// export { XGrid } from "./XGrid/";
+// export { YAxis } from "./YAxis/";
+// export { YGrid } from "./YGrid/";
+// export { Background } from "./Background/";
