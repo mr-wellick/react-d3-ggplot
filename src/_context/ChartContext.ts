634517f01c0411c7be2d75b67717ebbe2e6f772a
Pablo Lorenzo <lorenz3192@gmail.com>
2019-09-02 13:11:03 -0700

    reset context

--- src/_context/ChartContext.ts
+++ src/_context/ChartContext.ts
@@ -3,17 +3,13 @@ import { createContext } from "react";
 export interface IContext {
   data: any[];
   aes: string[];
-  dimensions?: { width: number; height: number; padding: number };
-  x_format?: string;
-  y_format?: string;
+  dimensions?: { width?: number; height?: number; padding?: number };
 }
 
 const ChartContext = createContext<IContext>({
   data: [],
   aes: [],
-  dimensions: { width: window.innerWidth, height: window.innerHeight, padding: 50 },
-  x_format: "",
-  y_format: ""
+  dimensions: {}
 });
 
 export default ChartContext;
