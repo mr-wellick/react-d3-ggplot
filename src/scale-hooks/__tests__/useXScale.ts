8a4f1b322b39188c266fac58e7aa87ff9d7f3e65
Pablo Lorenzo <lorenz3192@gmail.com>
2019-09-02 13:21:16 -0700

    removed tests

--- src/scale-hooks/__tests__/useXScale.ts
+++
@@ -1,3 +0,0 @@
-test("useXScale test", () => {
-  console.log("not sure how to test this hook yet.");
-});
--- src/scale-hooks/__tests__/useYScale.ts
+++
@@ -1,3 +0,0 @@
-test("useYScale test", () => {
-  console.log("not sure how to test this hook yet.");
-});
