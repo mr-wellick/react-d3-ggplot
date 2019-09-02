0bd2c508b4de3f9bfca421dca83e60b78af6c3db
Pablo Lorenzo <lorenz3192@gmail.com>
2019-09-02 16:42:59 -0700

    added time & ordinal scales

--- src/scale-hooks/use-x-scale.js
+++ src/scale-hooks/use-x-scale.js
@@ -1,6 +1,8 @@
 import { useContext } from 'react';
 import { ChartContext } from '../context/';
 import { LinearScale } from '../scale-utils/';
+import { TimeScale } from '../scale-utils/';
+import { OrdinalScale } from '../scale-utils/';
 
 function useScaleType() {
   const { data, aes } = useContext(ChartContext);
@@ -8,6 +10,16 @@ function useScaleType() {
   if (typeof data[0][aes[0]] === 'number') {
     const xValues = data.map(data => data[aes[0]]);
     const scale = new LinearScale(xValues).getScale();
+
+    return scale;
+  } else if (typeof data[0][aes[0]] === 'object') {
+    const xValues = data.map(data => data[aes[0]]);
+    const scale = new TimeScale(xValues).getScale();
+
+    return scale;
+  } else if (typeof data[0][aes[0]] === 'string') {
+    const xValues = data.map(data => data[aes[0]]);
+    const scale = new OrdinalScale(xValues).getScale(0.5); // will make argument modular later
 
     return scale;
   }
--- src/scale-hooks/use-y-scale.js
+++ src/scale-hooks/use-y-scale.js
@@ -1,6 +1,8 @@
 import { useContext } from 'react';
 import { ChartContext } from '../context/';
 import { LinearScale } from '../scale-utils/';
+import { TimeScale } from '../scale-utils/';
+import { OrdinalScale } from '../scale-utils/';
 
 function useScaleType() {
   const { data, aes } = useContext(ChartContext);
@@ -8,6 +10,16 @@ function useScaleType() {
   if (typeof data[1][aes[1]] === 'number') {
     const yValues = data.map(data => data[aes[1]]);
     const scale = new LinearScale(yValues).getScale();
+
+    return scale;
+  } else if (typeof data[1][aes[1]] === 'object') {
+    const yValues = data.map(data => data[aes[1]]);
+    const scale = new TimeScale(yValues).getScale();
+
+    return scale;
+  } else if (typeof data[1][aes[1]] === 'string') {
+    const yValues = data.map(data => data[aes[1]]);
+    const scale = new OrdinalScale(yValues).getScale(0.5); // will make argument modular later
 
     return scale;
   }
