e7282ca7e649835665bed2c96342b3d590df426b
Pablo Lorenzo <lorenz3192@gmail.com>
2019-09-13 17:02:17 -0700

    updated exports

--- src/scale-hooks/index.js
+++ src/scale-hooks/index.js
@@ -1,5 +1,4 @@
-export { default as useXScale } from './use-x-scale.js';
-export { default as useYScale } from './use-y-scale.js';
 export { default as useColors } from './use-colors.js';
 export { default as useSeries } from './use-series.js';
 export { default as useYScaleStack } from './use-y-scale-stack.js';
+export { default as useScale } from './use-scale.js';
